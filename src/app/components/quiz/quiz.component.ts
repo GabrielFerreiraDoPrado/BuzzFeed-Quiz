02e73850207a6e41882b69c07fa4334edb3a6721
Gabriel Ferreira do Prado <thegaprado@gmail.com>
2022-12-07 19:46:53 -0300

    Calculating result

--- src/app/components/quiz/quiz.component.ts
+++ src/app/components/quiz/quiz.component.ts
@@ -39,7 +39,7 @@ export class QuizComponent implements OnInit {
 
   playerChoice(value:string) {
         this.answers.push(value);
-        console.log(this.answers);
+        this.nextStep();
   }
 
   async nextStep() {
@@ -48,8 +48,25 @@ export class QuizComponent implements OnInit {
       if(this.questionMaxIndex > this.questionIndex) {
         this.questionSelected = this.questions[this.questionIndex];
       } else {
+        const finalAnswer:string = await this.checkResult(this.answers)
         this.finished = true;
+        this.answerSelected = quiz_questions.results[finalAnswer as keyof typeof quiz_questions.results];
       }
+  }
+
+  async checkResult(answers:string[]){
+    const result = answers.reduce((previous, current, i, arr) => {
+      if(
+        arr.filter(item => item === previous).length >
+        arr.filter(item => item === current).length
+      ){
+        return previous;
+      } else {
+        return current;
+      }
+    })
+    
+    return result;
 
   }
 
