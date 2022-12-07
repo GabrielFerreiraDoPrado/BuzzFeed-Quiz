3bce7e21af9dbc90ae3731be5c58edce973596c1
Gabriel Ferreira do Prado <thegaprado@gmail.com>
2022-12-07 19:04:28 -0300

    Logical properties

--- src/app/components/quiz/quiz.component.ts
+++ src/app/components/quiz/quiz.component.ts
@@ -5,7 +5,21 @@ import { Component, OnInit } from '@angular/core';
   templateUrl: './quiz.component.html',
   styleUrls: ['./quiz.component.css']
 })
+
 export class QuizComponent implements OnInit {
+
+  title:string = "";
+
+  questions:any;
+  questionSelected:any;
+
+  answers:string[] = [];
+  answerSelected:string = "";
+
+  questionIndex:number = 0;
+  questionMaxIndex:number = 0;
+
+  finished:boolean = false;
 
   constructor() { }
 
